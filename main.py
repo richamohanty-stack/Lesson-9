6043cc1f3a9dcce2e6834e55bcef39c8fa13331f
richamohanty-stack <richa.mohanty@gmail.com>
2025-12-26 09:51:07 +0530

    initial commit

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ age=int(input("Enter your age in years: "))
 if age>=10 and age<=20:
     print("Your age is in between 10 and 20 years")
     if age>=18:
-        print("and you are also eligible to vote.")
+        print(" and you are also eligible to vote.")
     else:
-        print("but, you are not eligible to vote.")
+        print(" but, you are not eligible to vote.")
 else:
     print("Your age is not in between 10 and 20 years")
     if age>=18:
-        print("but you are eligible to vote.")
+        print(" but you are eligible to vote.")
     else:
-        print("you are also elibible to vote.")
+        print(" and you are also elibible to vote.")
